bed11a757787f5c950323a633666824c190f1a6e
Sadu-sadullah <sadullah2000@gmail.com>
2025-03-29 12:35:54 +0530

    4

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "mailer.php",
                 type: "POST",
                 data: {
                     name: name,
